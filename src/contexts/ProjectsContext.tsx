6acc74f7c22bcca14fa7412b91e79326413b1abb
Matheus Alves <mtalves27@gmail.com>
2023-02-08 19:15:44 -0300

    feat: get projects from firebase

--- src/contexts/ProjectsContext.tsx
+++ src/contexts/ProjectsContext.tsx
@@ -1,9 +1,13 @@
 import React, { createContext, ReactNode, useEffect, useState } from 'react'
 
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+
 import { apiStates } from '../libs/axios'
+import { db } from '../services/firebase'
 
 interface ProjectsContextType {
   states: IStates[]
+  projects: IProjects[]
 }
 
 export const ProjectsContext = createContext({} as ProjectsContextType)
@@ -18,8 +22,38 @@ interface IStates {
   nome: string
 }
 
+interface IProjects {
+  id: string
+  client_name: string
+  created_at: string
+  phone: string
+  zipcode: string
+  state: string
+  city: string
+  street: string
+  neighborhood: string
+  number: number
+  complement: string
+  concessionaire: string
+  power: number
+}
+
 export function ProjectsProvider({ children }: ProjectsProviderProps) {
   const [states, setStates] = useState<IStates[]>([])
+  const [projects, setProjects] = useState<IProjects[]>([])
+
+  function getProjects() {
+    const q = query(collection(db, 'projects'), orderBy('created_at', 'desc'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const updatedProjects = querySnapshot.docs.map((doc) => {
+        const data = { ...doc.data(), id: doc.id } as IProjects
+
+        return data
+      })
+
+      setProjects(updatedProjects)
+    })
+  }
 
   async function fetchStates() {
     const response = await apiStates.get('/estados')
@@ -29,10 +63,11 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
 
   useEffect(() => {
     fetchStates()
+    getProjects()
   }, [])
 
   return (
-    <ProjectsContext.Provider value={{ states }}>
+    <ProjectsContext.Provider value={{ states, projects }}>
       {children}
     </ProjectsContext.Provider>
   )
--- src/pages/Projects/components/ProjectsTable.tsx
+++ src/pages/Projects/components/ProjectsTable.tsx
@@ -1,58 +1,80 @@
 import { PencilIcon } from '@heroicons/react/24/outline'
 import { TrashIcon } from '@heroicons/react/24/outline'
 
+import { Loading } from '../../../components/Loading'
+import { useProjects } from '../../../hooks/useProjects'
+
 export function ProjectsTable() {
+  const { projects } = useProjects()
+
   return (
-    <div className="mt-5 overflow-hidden overflow-x-auto border border-gray-200 rounded-lg shadow-md">
-      <table className="w-full text-sm text-left text-gray-500 bg-white">
-        <thead className="bg-orange-500">
-          <tr>
-            <th scope="col" className="px-6 py-4 font-medium text-white">
-              Cliente
-            </th>
-            <th scope="col" className="px-6 py-4 font-medium text-white">
-              Potência (kWp)
-            </th>
-            <th scope="col" className="px-6 py-4 font-medium text-white">
-              CEP
-            </th>
-            <th scope="col" className="px-6 py-4 font-medium text-white">
-              Telefone
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-4 font-medium text-gray-900"
-            ></th>
-          </tr>
-        </thead>
-        <tbody className="border-t border-gray-100 divide-y divide-gray-100">
-          <tr className="hover:bg-gray-50">
-            <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
-              <div className="text-sm">
-                <div className="font-medium text-gray-700">
-                  Dom João da Silva
-                </div>
-                <div className="text-gray-400 uppercase">Enel - CE</div>
-              </div>
-            </th>
-            <td className="px-6 py-4 w-[213px]">13.65</td>
-            <td className="px-6 py-4">62300-000</td>
-            <td className="px-6 py-4">
-              <div className="flex gap-2">(88) 99901-4007</div>
-            </td>
-            <td className="px-6 py-4">
-              <div className="flex justify-end gap-4">
-                <button>
-                  <TrashIcon className="w-6 h-6" />
-                </button>
-                <button>
-                  <PencilIcon className="w-6 h-6" />
-                </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      {projects.length === 0 ? (
+        <div className="py-28">
+          <Loading />
+        </div>
+      ) : (
+        <div className="mt-5 overflow-hidden overflow-x-auto border border-gray-200 rounded-lg shadow-md ">
+          <table className="w-full text-sm text-left text-gray-500 bg-white">
+            <thead className="bg-orange-500">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-white">
+                  Cliente
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-white">
+                  Potência (Wp)
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-white">
+                  CEP
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-white">
+                  Telefone
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-4 font-medium text-gray-900"
+                ></th>
+              </tr>
+            </thead>
+            {projects.map((project) => {
+              return (
+                <tbody
+                  key={project.id}
+                  className="border-t border-gray-100 divide-y divide-gray-100"
+                >
+                  <tr className="hover:bg-gray-50">
+                    <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                      <div className="text-sm">
+                        <div className="font-medium text-gray-700">
+                          {project.client_name}
+                        </div>
+                        <div className="text-gray-400 uppercase">
+                          {project.concessionaire}
+                        </div>
+                      </div>
+                    </th>
+                    <td className="px-6 py-4 w-[213px]">{project.power}</td>
+                    <td className="px-6 py-4">{project.zipcode}</td>
+                    <td className="px-6 py-4">
+                      <div className="flex gap-2">{project.phone}</div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex justify-end gap-4">
+                        <button>
+                          <TrashIcon className="w-6 h-6" />
+                        </button>
+                        <button>
+                          <PencilIcon className="w-6 h-6" />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              )
+            })}
+          </table>
+        </div>
+      )}
+    </>
   )
 }
